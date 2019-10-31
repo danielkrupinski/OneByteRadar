b34ac7dad40d6586a4d1f83bcd86d6e751f8d668
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-31 22:51:39 +0100

    Read and write byte as uchar instead of char

--- OneByteRadar.py
+++ OneByteRadar.py
@@ -9,5 +9,5 @@ clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
 address = client.lpBaseOfDll + re.search(rb'\x80\xB9.{5}\x74\x12\x8B\x41\x08',
                                          clientModule).start() + 6
 
-pm.write_char(address, chr(0 if ord(pm.read_char(address)) != 0 else 2))
+pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
 pm.close_process()
